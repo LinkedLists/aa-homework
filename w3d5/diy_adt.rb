b7e48f56326b4b6e5e76868413a96828d73295f2
appkenny <kzeng.95@gmail.com>
2020-12-04 04:45:31 -0800

    Finished map

--- w3d5/diy_adt.rb
+++ w3d5/diy_adt.rb
@@ -48,3 +48,57 @@ end
 # p q.enqueue("Sally")
 # p q.dequeue
 # p q.peek
+require "byebug"
+
+class Map
+    def initialize
+        @arr = []
+    end
+
+    def set(key, value)
+        # debugger
+        pair = [key, value]
+        if @arr == []
+            @arr << pair
+        else
+            @arr.map! do |ele| 
+                if ele[0] == key
+                    ele = pair
+                else
+                    ele
+                end
+            end
+            @arr << pair if !@arr.include?(pair)
+        end
+    end
+
+    def get(key)
+        @arr.select { |pair| pair[0] == key }
+    end
+
+    def delete(key)
+        @arr.map! do |pair| 
+            if pair[0] == key
+                pair = nil 
+            else
+                pair
+            end
+        end
+        @arr.compact!
+    end
+
+    def show
+        print @arr
+        puts ""
+    end
+
+end
+
+# map = Map.new
+# p map.set("k1", "v1")
+# p map.set("k2", "v2")
+# p map.set("k3", "v3")
+# p map.get("k1")
+# map.delete("k1")
+# map.show
+
