d6e81e3d34547b37679ec4cc4a963ab84de6dd09
appkenny <kzeng.95@gmail.com>
2020-12-08 04:18:01 -0800

    Attempted phase 1

--- w4d2/skeleton/super_useful.rb
+++ w4d2/skeleton/super_useful.rb
@@ -1,47 +1,50 @@
 # PHASE 2
 def convert_to_int(str)
     Integer(str)
-  end
+end
   
   # PHASE 3
-  FRUITS = ["apple", "banana", "orange"]
-  
-  def reaction(maybe_fruit)
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
     if FRUITS.include? maybe_fruit
-      puts "OMG, thanks so much for the #{maybe_fruit}!"
+        puts "OMG, thanks so much for the #{maybe_fruit}!"
     else 
-      raise StandardError 
+        raise StandardError.new "thats not a fruit -.-"
     end 
-  end
-  
-  def feed_me_a_fruit
-    puts "Hello, I am a friendly monster. :)"
-  
+end
+
+def feed_me_a_fruit
+    puts "Hello, I am a friendly monster. :)"   
     puts "Feed me a fruit! (Enter the name of a fruit:)"
     maybe_fruit = gets.chomp
-    reaction(maybe_fruit) 
-  end  
-  
-  # PHASE 4
-  class BestFriend
+    begin 
+        reaction(maybe_fruit)
+    rescue StandardError => e
+        puts "i want coffee now"
+        maybe_fruit = gets.chomp
+        reaction(maybe_fruit)
+        retry
+    end
+end  
+
+# PHASE 4
+class BestFriend
     def initialize(name, yrs_known, fav_pastime)
-      @name = name
-      @yrs_known = yrs_known
-      @fav_pastime = fav_pastime
-    end
-  
+        @name = name
+        @yrs_known = yrs_known
+        @fav_pastime = fav_pastime
+    end 
     def talk_about_friendship
-      puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
-    end
-  
+        puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    end 
     def do_friendstuff
-      puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
-    end
-  
+        puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+    end 
     def give_friendship_bracelet
-      puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+        puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
     end
-  end
+end
   
   
   
