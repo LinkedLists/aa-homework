c890ecd3d49d2150dddbfedf607a68dfea7b7827
appkenny <kzeng.95@gmail.com>
2020-12-10 06:02:45 -0800

    Finished 1st attempt

--- w4d4/spec/dessert_spec.rb
+++ w4d4/spec/dessert_spec.rb
@@ -14,37 +14,50 @@ describe Dessert do
       expect(dessert.type).to_not eq(nil)
     end
     it "sets a quantity" do
-      expect(dessert.quantity.class).to eq(Integer) 
+      expect(dessert.quantity).to eq(500) 
     end
     it "starts ingredients as an empty array" do
       expect(dessert.ingredients).to eq([])
     end
     it "raises an argument error when given a non-integer quantity" do 
-      expect(dessert.quantity).to eq(Integer) 
+      expect(dessert.quantity.class).to eq(Integer) 
     end
   end
 
   describe "#add_ingredient" do
     it "adds an ingredient to the ingredients array" do
-      expect(dessert.ingredients[-1])
+      dessert.add_ingredient("Eggs")
+      expect(dessert.ingredients[-1]).to eq("Eggs")
     end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    let(:temp) {dessert.ingredients}
+    it "shuffles the ingredient array" do
+      expect(temp).to_not eq(dessert.ingredients)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
+    it "subtracts an amount from the quantity" do
+    dessert.eat(20)
+    expect(dessert.quantity).to eq(480)
+    end
+    it "raises an error if the amount is greater than the quantity" do
+      expect {dessert.quantity.to raise-error(ArgumentError)}
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(dessert.serve).to include(dessert.chef.titleize)
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      dessert.make_more
+      expect(dessert.temp).to be(400)
+    end
   end
 end
