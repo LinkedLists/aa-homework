ef0a32c8ef97482ca8c114957f486a48711dd2aa
appkenny <kzeng.95@gmail.com>
2021-01-05 03:58:13 -0800

    stuck on action mailer

--- w7d2/helper_layout_partials/app/mailers/user_mailer.rb
+++ w7d2/helper_layout_partials/app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: 'ninetyninecatsadmin@example.com'
 
-  def welcome_email(user)
+  def welcome_email(user) #here by default?
     @user = user 
     @url = 'http://localhost:3000/session/new'
     mail(to: @user.username, subject:'Welcome to 99 Cats!')
