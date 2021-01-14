c581ec447703a11b2e3446d4e031958600927c29
appkenny <kzeng.95@gmail.com>
2021-01-14 04:00:37 -0800

    completed homework

--- w8d4/homework.js
+++ w8d4/homework.js
@@ -28,7 +28,7 @@ Elephant.prototype.addTrick = function(trick) {
 }
 
 Elephant.prototype.play = function() {
-  random = Math.floor(Math.random(this.tricks.length + 1));
+  random = Math.floor(Math.random(this.tricks.length + 1)); //not getting random int
   console.log(random);
 }
 
@@ -38,3 +38,30 @@ Elephant.prototype.play = function() {
 // elephant.addTrick("jumping");
 // elephant.play();
 
+
+function paradeHelper(elephant) {
+  console.log(elephant.name + " is an elephant");
+}
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+// herd.forEach(paradeHelper);
+
+
+function dinerBreakfast() {
+  let order = " 100x bacon";
+
+  return function haha(extras) {
+    return (order + " and an order of " + extras);
+  }
+}
+
+
+let bfastOrder = dinerBreakfast();
+console.log(bfastOrder("chocolate chip pancakes"));
+console.log(bfastOrder("grits"));
